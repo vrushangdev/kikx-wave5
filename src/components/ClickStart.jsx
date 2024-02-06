6c8c84ed17f9aeb4b852968884eb863e6e0b1042
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-02-07 03:25:44 +0530

    new

--- src/components/ClickStart.jsx
+++ src/components/ClickStart.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 function ClickStart() {
   return (
-    <section className=" relative -mt-[5%] sm:-mt-[1%]">
-        <div className=" mx-auto  justify-center mt-5 mb-2  flex">
+    <section className=" relative -mt-[5%] sm:-mt-[1%] btn-hov">
+      <img src="images/btn.png" alt="" className=" absolute top-0 left-0 right-0" />
+        <div className=" mx-auto  justify-center mt-5 mb-2  flex relative z-10">
             <a
               href="https://test.kix.digital/?utm_source=landingpage&utm_medium=website&utm_campaign=tradeathon&utm_content=wave3&utm_term=1millionkixtokens" 
-              className="  tracking-[-0.11em] uppercase archo-black text-5xl  sm:text-6xl  py-2 px-4 sm:px-6   bg-[#fbe000] border-2 border-[#000]   mx-auto btn-hov rounded-xl  text-[#94339d]"
+              className="  tracking-[-0.11em] -mt-1 uppercase archo-black text-5xl  sm:text-6xl  py-2 px-4 sm:px-6        mx-auto    text-[#94339d]"
             >
               {" "}
-              Play <span>Now</span>
+              Click to Play
               {/* <img src="images/click.png" alt="" className=" h-[34px] sm:h-[45px]" /> */}
             </a>
           </div>
