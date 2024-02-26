99af10eadd132a3c8d6e9740d0c281235ca6d4ac
Vrushang Desai <45510867+vrushangdev@users.noreply.github.com>
2024-02-26 15:28:35 +0530

    Onboarding Path Updated

--- src/components/ClickStart.jsx
+++ src/components/ClickStart.jsx
@@ -6,7 +6,7 @@ function ClickStart() {
       <img src="images/btn.png" alt="" className=" absolute top-0 left-0 right-0" />
         <div className=" mx-auto  justify-center mt-5 mb-2  flex relative z-10">
             <a
-              href="https://test.kix.digital/?utm_source=landingpage&utm_medium=website&utm_campaign=tradeathon&utm_content=wave3&utm_term=1millionkixtokens" 
+              href="https://test.kix.digital/onboarding?utm_source=landingpage&utm_medium=website&utm_campaign=tradeathon&utm_content=wave3&utm_term=1millionkixtokens" 
               className="  tracking-[-0.11em] -mt-2 sm:-mt-1 uppercase archo-black text-4xl  sm:text-6xl  py-2 px-4 sm:px-6        mx-auto    text-[#94339d]"
             >
               {" "}
